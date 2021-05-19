311aa2362f74919bcf7fc7f04c81d0282cb8573e
ARSALAN UMAR <64530489+Arxlan40@users.noreply.github.com>
2021-05-19 16:30:24 +0500

    New

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:eplaza/awein.dart';
 import 'package:eplaza/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -9,7 +8,6 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 
-import 'Notification.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
